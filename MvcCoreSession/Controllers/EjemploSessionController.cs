8ab36ce01654d320621195daff1344eea1581895
DPG210 <diegogrego210@gmail.com>
2026-02-18 10:05:47 +0100

    Add project files.

---
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreSession.Controllers
+{
+    public class EjemploSessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SessionSimple(string accion)
+        {
+            if(accion != null)
+            {
+                if (accion.ToLower() == "almacenar") 
+                {
+                    //GUARDAMOS DATOS EN SESSION
+                    HttpContext.Session.SetString("nombre", "Programeitor");
+                    HttpContext.Session.SetString("hora", DateTime.Now.ToLongTimeString());
+                    ViewData["MENSAJE"] = "DATOS ALMACENADOS EN LA SESSION";
+                }else if (accion.ToLower() == "mostrar")
+                {
+                    //RECUPERAMOS LOS DATOS DE SESSION
+                    ViewData["NOMBRE"] = HttpContext.Session.GetString("nombre");
+                    ViewData["HORA"] = HttpContext.Session.GetString("hora");
+                }
+            }
+            return View();
+        }
+    }
+}
