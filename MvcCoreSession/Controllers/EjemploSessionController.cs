2f9cccfda24a2d242741d6d7462fd570145cdf14
DPG210 <diegogrego210@gmail.com>
2026-02-18 13:43:23 +0100

    session de objetos

--- MvcCoreSession/Controllers/EjemploSessionController.cs
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreSession.Models;
+using MvcCoreSession.Helpers;
+using System.Runtime.Serialization;
 
 namespace MvcCoreSession.Controllers
 {
@@ -6,6 +9,12 @@ namespace MvcCoreSession.Controllers
     {
         public IActionResult Index()
         {
+            if (HttpContext.Session.Get("MASCOTA") != null)
+            {
+                byte[] data = HttpContext.Session.Get("MASCOTA");
+                Mascota mascota = (Mascota)Helpers.HerlperBinarySession.ByteToObject(data);
+                ViewData["MASCOTA"] = mascota;
+            }
             return View();
         }
         public IActionResult SessionSimple(string accion)
@@ -23,6 +32,67 @@ namespace MvcCoreSession.Controllers
                     //RECUPERAMOS LOS DATOS DE SESSION
                     ViewData["NOMBRE"] = HttpContext.Session.GetString("nombre");
                     ViewData["HORA"] = HttpContext.Session.GetString("hora");
+                }
+            }
+            return View();
+        }
+        public IActionResult SessionMascotaBytes(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    //GUARDAMOS DATOS EN SESSION
+                    Mascota mascota = new Mascota();
+                    mascota.Nombre = "Wall-E";
+                    mascota.Raza = "Cleaner";
+                    mascota.Edad = 18;
+                    //Para almacenar la mascota en Session, debemos
+                    //convertirlo a byte[]
+                   
+                    byte[] data = HerlperBinarySession.ObjectToByte(mascota);
+       
+                                  //ALMACENAMOS EL OBJETO EN SESSION
+                    HttpContext.Session.Set("MASCOTA", data);
+                    ViewData["MENSAJE"] = "MASCOTA ALMACENADA";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    //RECUPERAMOS LOS DATOS DE MASCOTA
+                    //EN BYTES QUE TENEMOS EN SESSION
+                    byte[] data = HttpContext.Session.Get("MASCOTA");
+                    //CONVERTIMOS BYTES A OBJECT
+                    Mascota mascota = (Mascota)Helpers.HerlperBinarySession.ByteToObject(data);
+                    //PARA REPRESENTARLO DE FORMA VISUAL, LO ENVIAMOS 
+                    //A VIEWDATA
+                    ViewData["MASCOTA"] = mascota;
+                }
+            }
+            return View();
+        }
+        public IActionResult SessionMascotaCollectionBytes(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    //GUARDAMOS DATOS EN SESSION
+                    List<Mascota> mascotasList = new List<Mascota>
+                    {
+                        new Mascota{Nombre= "Nala", Raza="Leona", Edad= 21},
+                        new Mascota{Nombre= "Sebastian", Raza="cangrejo", Edad= 24},
+                        new Mascota{Nombre= "Rafiki", Raza="Brujo", Edad= 23},
+                        new Mascota{Nombre= "Olaf", Raza="Nieve", Edad= 210}
+                    };
+                    byte[] data = Helpers.HerlperBinarySession.ObjectToByte(mascotasList);
+                    HttpContext.Session.Set("Mascotas", data);
+                    ViewData["MENSAJE"] = "Coleccion almacenada";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    byte[] data = HttpContext.Session.Get("Mascotas");
+                    List<Mascota> mascotasList = (List<Mascota>)Helpers.HerlperBinarySession.ByteToObject(data);
+                    return View(mascotasList);
                 }
             }
             return View();
---
+++ MvcCoreSession/Helpers/HerlperBinarySession.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MvcCoreSession.Helpers
+{
+    public  class HerlperBinarySession
+    {
+        //VAMOS A CREAR LOS METODOS DE TIPO static
+        //PORQUE PARA CONVERTIR NO VO VOY A UTILIZAR NADA DE ESTA CLASE
+        //SOLO LA FUNCIONALIDAD
+        //CONVERTIMOS OBJETO A BYTE[]
+        public static byte[] ObjectToByte(Object objeto)
+        {
+
+        #pragma warning disable SYSLIB0011 // Type or member is obsolete
+            BinaryFormatter formatter =
+                new BinaryFormatter();
+        #pragma warning restore SYSLIB0011 // Type or member is obsolete
+        #pragma warning restore SYSLIB0011
+            using (MemoryStream stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, objeto);
+                return stream.ToArray();
+            }
+        }
+        //CONVERTIMOS DE BYTE[] A OBJETO
+        public static Object ByteToObject(byte[] data)
+        {
+        #pragma warning disable SYSLIB0011 // Type or member is obsolete
+            BinaryFormatter formatter =
+                new BinaryFormatter();
+        #pragma warning restore SYSLIB0011 // Type or member is obsolete
+        #pragma warning restore SYSLIB0011
+            using (MemoryStream stream = new MemoryStream())
+            {
+                stream.Write(data, 0, data.Length);
+                stream.Seek(0, SeekOrigin.Begin);
+                Object objeto = (Object)
+                    formatter.Deserialize(stream);
+                return objeto;
+            }
+        }
+    }
+}
